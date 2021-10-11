9a6ab7499b8e18bb293831b65c2fa3023803d0db
ShaanHash <hashmishaana@gmail.com>
2021-10-11 15:48:35 -0400

    Grid made

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let con = document.querySelector(".container");
-let cellNumber = window.prompt("Number of Squares");
+let size = window.prompt('How many cells per side?');
+
+if (size > 50) {
+    alert("Max Size 50 cells")
+    size = window.prompt("How many cells per side?")
+};
+
+
+size = size * 20
+
+con.style.height = `${size}px`;
+con.style.width = `${size}px`;
+
+
+let cellNumber = Math.pow(Math.floor(size/20),2);
+
+
 
 
 for (let i = 0; i < cellNumber; i++) {
     let cell = document.createElement("div");
-    console.log(i);
-    cell.innerText = "CELL";
+    cell.classList.add("cell");
     con.appendChild(cell); 
+    con.classList.add("container2");
 };
